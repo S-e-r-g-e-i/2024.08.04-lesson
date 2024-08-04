fbc38bbd0ee19b902f0f93f47f12a890211d8ea7
S-e-r-g-e-i <s.ya.aronov.b@gmail.com>
2024-08-04 22:02:40 +0500

    Add files via upload

---
+++ task.py
@@ -0,0 +1,12 @@
+#1 Динамическая типизация данных (создал отдельный проект, осознанно, для удобства упорядоченного хранения)
+name = 'Sergei' #2
+print('Name: ' + name)  #3
+age = 35    #4
+print('Age:', age) #5.1
+print('Age: ' + str(age))   #5.2 или так, чтобы полностью управлять пробелами
+age = age + 1   #6
+print("New age:", age)  #7.1
+print("New age: " + str(age))  #7.2 или так, чтобы полностью управлять пробелами
+is_student = True   #8
+print('Is student:', is_student)    #9.1
+print('Is student: ' + str(is_student))    #9.2  или так, чтобы полностью управлять пробелами
